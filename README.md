a862758189b4811a056c1a3aead033b63f470982
Andrii Siriak <siryaka@gmail.com>
2019-08-18 16:58:36 +0300

    Add toast notifications, optimize code

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Clipboard Image Auto Saver is a Python script which monitors the Windows' clipboard and automatically saves images from there.
 
 ## Usage
-1. Install required packages pywin32 and pillow using package manager [pip](https://pip.pypa.io/en/stable/)
+1. Install required packages using package manager [pip](https://pip.pypa.io/en/stable/)
 ```bash
-pip install pillow pywin32
+pip install pillow pywin32 win10toast
 ```
 2. Run the start.bat file. The batch file is provided to launch the python script in background.
 3. Add shortcut to start.bat in C:\Users\\<your user>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup. Windows will start script automatically on Windows load.
--- clipboard_img_auto_saver.py
+++ clipboard_img_auto_saver.py
@@ -3,41 +3,39 @@ from PIL.ImageGrab import grabclipboard
 from PIL.Image import new
 from win32clipboard import IsClipboardFormatAvailable
 from time import sleep
+from win10toast import ToastNotifier
+
+toaster = ToastNotifier()
 
 def run_loop():
-    old_img_list = ''
+    old_img_list = None
     while True:  # The event loop that checks every 10 seconds for new image
         try:
             if img_in_clipboard():
                 new_img = grabclipboard()
-                if img_changed(new_img, old_img_list):
-                    old_img_list = list(new_img.getdata())
-                    process_clipboard(new_img)
+                new_img_list = list(new_img.getdata())
+                if img_changed(new_img_list, old_img_list):
+                    save_img(new_img)
+                    old_img_list = new_img_list
+                    send_message('Image saved', 3)
         except Exception as e:
-            print('An error occured', e)
+            send_message('Error occured: ' + e.args[0], 3600)
         sleep(10)
 
 def img_in_clipboard():
     return IsClipboardFormatAvailable(8)
 
-def img_changed(new_img, old_img_list):
-    if old_img_list == '':
+def img_changed(new_img_list, old_img_list):
+    if old_img_list is None:
         return True
-    return list(new_img.getdata()) != old_img_list
-
-def process_clipboard(new_img):
-    tim = keep_transparency(new_img)
-    save_img(tim)
-
-def keep_transparency(im):
-    im = im.convert("RGBA")
-    tim = new('RGBA', im.size, (255,255,255,0))
-    tim.paste(im, (0,0), mask=im.split()[3])
-    return tim   
+    return new_img_list != old_img_list
 
 def save_img(im):
     file_timestamp = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
     im.save('E:\\OneDrive\\Images\\' + file_timestamp + '.png', 'PNG')
+
+def send_message(msg, dur):
+    toaster.show_toast('Clipboard image saver', msg, duration=dur)
 
 if __name__ == '__main__':
      run_loop()
